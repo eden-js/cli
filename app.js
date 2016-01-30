0ca9f86db843b78115be8bd96fec0732daf4e63d
edencoder <imthebanana@live.com>
2016-01-30 13:34:04 +1000

    begin rewrite into ES6

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import express      from 'express';
+import path         from 'path';
+import favicon      from 'favicon';
+import logger       from 'logger';
+import cookieParser from 'cookie-parser';
+import bodyParser   from 'body-parser';
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
