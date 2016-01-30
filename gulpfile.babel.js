b01b12b645f5bfcf2f9bb9d07cea8a5666c17ae6
edencoder <imthebanana@live.com>
2016-01-30 15:55:24 +1000

    move grunt routing function to utils

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import rename  from 'gulp-rename';
 import sass    from 'gulp-sass';
 import parse   from 'comment-parser';
 import through from 'through2';
+import routing from './bin/util/gulp.routing';
 import fs      from 'fs';
 
 // gulp sass task
@@ -32,65 +33,8 @@ gulp.task('routes:watch', function () {
 gulp.task('routes', function () {
     var allRoutes = {};
     gulp.src('./app/bundles/**/*Controller.js')
-        .pipe(through.obj(function (chunk, enc, cb) {
-            var content = chunk.contents.toString();
-            var parsed  = parse(content);
-            var lines   = content.split('\n');
-            var routes  = {};
-
-            for (var key in parsed) {
-                if (parsed[key].tags.length) {
-                    if (parsed[key]) {
-                        // null variables
-                        var fn = false;
-                        var i  = parsed[key].line;
-
-                        // loop lines for function
-                        while (!fn) {
-                            i++;
-                            if (!lines[i]) {
-                                break;
-                            }
-                            if (lines[i].indexOf('(') > -1 && lines[i].indexOf(')') > -1 && lines[i].indexOf('*') == -1) {
-                                fn = lines[i].split('(')[0].trim();
-                            }
-                        }
-
-                        // continue if function not found
-                        if (!fn) {
-                            continue;
-                        }
-
-                        // loop tags for route
-                        for (var tag in parsed[key].tags) {
-                            // set tag
-                            tag = parsed[key].tags[tag];
-
-                            // ensure tag route
-                            if (tag.tag != 'route') {
-                                continue;
-                            }
-
-                            // insert route array
-                            if (!routes[tag.type]) {
-                                routes[tag.type] = {};
-                            }
-
-                            // set route function
-                            routes[tag.type][tag.name] = {
-                                'controller' : '/app/bundles' + chunk.path.split('bundles')[1].replace(/\\/g, '/'),
-                                'action'     : fn
-                            };
-                        }
-                    }
-                }
-            }
-
-            this.push({ routes : routes });
-            cb(null, chunk);
-        }))
+        .pipe(through.obj(routing))
         .on('data', function (data) {
-            console.log(data);
             MergeRecursive(allRoutes, data.routes);
         })
         .on('end', function() {
