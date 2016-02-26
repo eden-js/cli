52fc4083eb63053af2b852bcd6f1ff75c8dc33d5
Alex <alex.taylor@ifactory.com.au>
2016-02-26 15:42:47 +1000

    refactor gruntfile into standard ES6 format

--- gulpfile.js
+++ gulpfile.js
@@ -2,280 +2,318 @@
  * Created by Awesome on 1/30/2016.
  */
 
-// use strict
+    // use strict
 'use strict';
 
 // import dependencies
-var gulp       = require('gulp');
-var glob       = require('glob');
-var rename     = require('gulp-rename');
-var sass       = require('gulp-sass');
-var through    = require('through2');
-var path       = require('path');
-var routing    = require('./bin/util/gulp.routing');
-var menus      = require('./bin/util/gulp.menus');
-var fs         = require('fs');
-var del        = require('del');
-var server     = require('gulp-express');
-var minifyCss  = require('gulp-minify-css');
-var sourcemaps = require('gulp-sourcemaps');
-var browserify = require('browserify');
-var babelify   = require('babelify');
-var source     = require('vinyl-source-stream');
-var concat     = require('gulp-concat');
-var riot       = require('gulp-riot');
-var insert     = require('gulp-insert');
-var streamify  = require('gulp-streamify');
-var uglify     = require('gulp-uglify');
+var glob       = require ('glob');
+var rename     = require ('gulp-rename');
+var sass       = require ('gulp-sass');
+var through    = require ('through2');
+var path       = require ('path');
+var fs         = require ('fs');
+var server     = require ('gulp-express');
+var sourcemaps = require ('gulp-sourcemaps');
+var browserify = require ('browserify');
+var babelify   = require ('babelify');
+var source     = require ('vinyl-source-stream');
+var concat     = require ('gulp-concat');
+var riot       = require ('gulp-riot');
+var insert     = require ('gulp-insert');
+var streamify  = require ('gulp-streamify');
+var uglify     = require ('gulp-uglify');
+
+// import local dependencies
+var routePipe = require ('./bin/util/gulp.routing');
+var menuPipe  = require ('./bin/util/gulp.menus');
 
 /**
- * GULP TASKS
+ * build gulp builder class
  */
+class gulpBuilder {
+    /**
+     * construct gulp builder class
+     */
+    constructor () {
+        // bind variables
+        this.gulp = require ('gulp');
 
-// gulp sass task
-gulp.task('sass', function () {
-    var allSass = '';
-    gulp.src(['node_modules/bootstrap/scss/bootstrap-flex.scss', './bin/bundles/*/resources/scss/bootstrap.scss', './app/bundles/*/resources/scss/bootstrap.scss'])
-        .pipe(through.obj(function(chunk, enc, cb) {
-            allSass = allSass + '@import ".' + chunk.path.replace(__dirname, '').split('\\').join('/') + '"; ';
+        // bind methods
+        this._watch = [];
+        this._tasks = {
+            'sass' : [
+                'node_modules/bootstrap/scss/bootstrap-flex.scss',
+                './bin/bundles/*/resources/scss/bootstrap.scss',
+                './app/bundles/*/resources/scss/bootstrap.scss'
+            ],
+            'daemon' : [
+                './bin/bundles/**/*Daemon.js',
+                './app/bundles/**/*Daemon.js'
+            ],
+            'route' : [
+                './bin/bundles/**/*Controller.js',
+                './app/bundles/**/*Controller.js'
+            ],
+            'menu' : [
+                './bin/bundles/**/*Controller.js',
+                './app/bundles/**/*Controller.js'
+            ],
+            'view' : [
+                './bin/bundles/*/view/**/*.hbs',
+                './app/bundles/*/view/**/*.hbs'
+            ],
+            'tag' : [
+                './bin/bundles/*/view/**/*.tag',
+                './app/bundles/*/view/**/*.tag'
+            ],
+            'js' : [
+                './bin/bundles/*/resources/js/bootstrap.js',
+                './app/bundles/*/resources/js/bootstrap.js'
+            ]
+        };
 
-            this.push({ allSass : allSass });
-            cb(null, chunk);
-        }))
-        .on('data', function (data) {
-            // do nothing
-        })
-        .on('end', function () {
-            fs.writeFile('./tmp.scss', allSass, function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-                gulp.src('./tmp.scss')
-                    .pipe(sourcemaps.init())
-                    .pipe(sass({outputStyle: 'compressed'}))
-                    .pipe(rename('app.min.css'))
-                    .pipe(sourcemaps.write('./'))
-                    .pipe(gulp.dest('./www/assets/css'))
-                    .on('end', function() {
-                        fs.unlinkSync('./tmp.scss');
-                    });
+        // set keys array
+        var that  = this;
+        var keys  = Object.keys(this._tasks);
+        var watch = [];
+
+        // bind and add gulp task methods
+        for (var i = 0; i < keys.length; i ++) {
+            // bind method
+            this[keys[i]] = this[keys[i]].bind (this);
+            // setup task
+            this.gulp.task (keys[i], this[keys[i]]);
+            // setup watch task
+            this.gulp.task (keys[i] + ':watch', () => {
+                that.gulp.watch (that._tasks[keys[i]], [keys[i]]);
             });
-        })
-});
+            // add watch task to array
+            watch.push(keys[i] + ':watch');
+        }
 
-// gulp daemons task
-gulp.task('daemons', function() {
-    var entries = glob.sync('./bin/bundles/**/*Daemon.js');
-        entries = entries.concat(glob.sync('./app/bundles/**/*Daemon.js'));
+        this.gulp.task('install', keys);
+        this.gulp.task('watch', watch);
+        this.gulp.task('dev', () => {
+            server.run ([
+                './app.js'
+            ]);
 
-    for (var key in entries) {
-        entries[key] = entries[key].replace('./', '/');
+            for (var i = 0; i < keys.length; i++) {
+                that.gulp.watch (that._tasks[keys[i]], [keys[i], (event) => {
+                    // check for server
+                    server.notify (event);
+                }]);
+            }
+        });
+        this.gulp.task('default', ['dev']);
     }
 
-    fs.writeFile('./cache/daemons.json', JSON.stringify(entries), function (err) {
-        if (err) {
-            return console.log(err);
+    /**
+     * sass task
+     */
+    sass () {
+        // set variables
+        var that = this;
+        var all  = '';
+
+        // grab gulp source for sass
+        this.gulp.src (
+            this._tasks['sass']
+        ).pipe (through.obj ((chunk, enc, cb) => {
+                // add @import to all
+                all += '@import ".' + chunk.path.replace (__dirname, '').split (path.delimiter).join ('/') + '"; ';
+
+                // run through callback
+                cb (null, chunk);
+            })).on ('end', () => {
+                // write temp sass file
+                fs.writeFile ('./tmp.scss', all, (err) => {
+                    if (err) {
+                        console.log (err);
+                        return;
+                    }
+
+                    // pipe temp sass file for sass function
+                    that.gulp.src ('./tmp.scss')
+                        .pipe (sourcemaps.init ())
+                        .pipe (sass ({
+                            outputStyle : 'compressed'
+                        })).pipe (rename ('app.min.css'))
+                        .pipe (sourcemaps.write ('./'))
+                        .pipe (gulp.dest ('./www/assets/css'))
+                        .on ('end', () => {
+                            fs.unlinkSync ('./tmp.scss');
+                        });
+                });
+            });
+    }
+
+    /**
+     * daemon task
+     */
+    daemon () {
+        // grab daemon controllers
+        var daemons = [];
+        for (var i = 0; i < this._tasks['daemon'].length; i++) {
+            daemons = daemons.concat(glob.sync (this._tasks['daemon'][i]));
         }
-    });
-});
 
-// gulp routes task
-gulp.task('routes', function () {
-    var allRoutes = {};
-    gulp.src('./app/bundles/**/*Controller.js')
-        .pipe(through.obj(routing))
-        .on('data', function (data) {
-            MergeRecursive(allRoutes, data.routes);
-        })
-        .on('end', function () {
-            fs.writeFile('./cache/routes.json', JSON.stringify(allRoutes), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
+        // loop daemons
+        for (var key in daemons) {
+            daemons[key] = daemons[key].replace ('./', '/');
+        }
+
+        // write daemons cache file
+        fs.writeFile ('./cache/daemons.json', JSON.stringify (daemons), function (err) {
+            if (err) {
+                return console.log (err);
+            }
         });
-});
+    }
 
-// gulp routes task
-gulp.task('menus', function () {
-    var allMenus = {};
-    gulp.src('./app/bundles/**/*Controller.js')
-        .pipe(through.obj(menus))
-        .on('data', function (data) {
-            MergeRecursive(allMenus, data.menus);
-        })
-        .on('end', function () {
-            fs.writeFile('./cache/menus.json', JSON.stringify(allMenus), function (err) {
-                if (err) {
-                    return console.log(err);
-                }
+    /**
+     * route task
+     */
+    route() {
+        // set variables
+        var that = this;
+        var all  = {};
+
+        // get all routes
+        this.gulp.src (
+            this._tasks['route']
+        )
+            .pipe (through.obj (routePipe))
+            .on ('data', (data) => {
+                that._merge(all, data.routes);
+            }).on ('end', function () {
+                fs.writeFile ('./cache/routes.json', JSON.stringify (all), function (err) {
+                    if (err) {
+                        return console.log (err);
+                    }
+                });
             });
-        });
-});
+    }
 
-// gulp views task
-gulp.task('views', function() {
-    gulp.src(['./bin/bundles/*/view/**/*.hbs', './app/bundles/*/view/**/*.hbs'])
-        .pipe(rename(function(filePath) {
-            var amended = filePath.dirname.split(path.sep);
-            amended.shift();
-            amended.shift();
-            filePath.dirname = amended.join(path.sep);
-        }))
-        .pipe(gulp.dest('cache/view'));
-});
+    /**
+     * menu task
+     */
+    menu() {
+        // set variables
+        var that = this;
+        var all  = {};
 
-// gulp tags task
-gulp.task('tags', function() {
-    gulp.src(['./bin/bundles/*/view/**/*.tag', './app/bundles/*/view/**/*.tag'])
-        .pipe(rename(function(filePath) {
-            var amended = filePath.dirname.split(path.sep);
-            amended.shift();
-            amended.shift();
-            amended.shift();
-            filePath.dirname = amended.join(path.sep);
-        }))
-        .pipe(riot({
-            compact: true
-        }))
-        .pipe(concat('tags.min.js'))
-        .pipe(insert.prepend('var riot = require(\'riot\');'))
-        .pipe(gulp.dest('./cache/tag'));
-});
+        // get all menus
+        this.gulp.src (
+            this._tasks['menu']
+        )
+            .pipe (through.obj (menuPipe))
+            .on ('data', function (data) {
+                that._merge (all, data.menus);
+            }).on ('end', function () {
+                fs.writeFile ('./cache/menus.json', JSON.stringify (all), function (err) {
+                    if (err) {
+                        return console.log (err);
+                    }
+                });
+            });
+    }
 
-// gulp routes task
-gulp.task('javascript', function () {
-    var entries = glob.sync('./bin/bundles/*/resources/js/bootstrap.js');
-        entries = entries.concat(glob.sync('./app/bundles/*/resources/js/bootstrap.js'));
+    /**
+     * view task
+     */
+    view() {
+        // move views into single folder
+        // @todo bundle prioroty
+        this.gulp.src (
+            this._tasks['view']
+        )
+            .pipe (rename (function (filePath) {
+                var amended = filePath.dirname.split (path.sep);
+                amended.shift ();
+                amended.shift ();
+                filePath.dirname = amended.join (path.sep);
+            }))
+            .pipe (this.gulp.dest ('cache/view'));
+    }
 
-    browserify({
-        entries : entries
-    })
-        .transform(babelify)
-        .bundle()
-        .pipe(source('app.min.js'))
-        .pipe(insert.prepend(fs.readFileSync('./node_modules/bootstrap/dist/js/bootstrap.js')))
-        .pipe(insert.prepend(fs.readFileSync('./node_modules/jquery/dist/jquery.min.js')))
-        .pipe(streamify(uglify()))
-        .pipe(gulp.dest('./www/assets/js'));
-});
+    /**
+     * tag task
+     */
+    tag() {
+        // move tags into javascript compiled file (riotjs)
+        this.gulp.src (
+            this._tasks['tag']
+        )
+            .pipe (rename (function (filePath) {
+                var amended      = filePath.dirname.split (path.sep);
+                amended.shift ();
+                amended.shift ();
+                amended.shift ();
+                filePath.dirname = amended.join (path.sep);
+            }))
+            .pipe (riot ({
+                compact : true
+            }))
+            .pipe (concat ('tags.min.js'))
+            .pipe (insert.prepend ('var riot = require(\'riot\');'))
+            .pipe (gulp.dest ('./cache/tag'));
+    }
 
-/**
- * GULP WATCH TASKS
- */
+    /**
+     * javascript task
+     */
+    js() {
+        // create javascript array
+        var js = [];
+        for (var i = 0; i < this._tasks['js'].length; i++) {
+            js = js.concat(glob.sync (this._tasks['js'][i]));
+        }
 
-// gulp sass watch task
-gulp.task('sass:watch', function () {
-    gulp.watch('./app/bundles/**/*.scss', ['sass']);
-});
+        // browserfiy javascript
+        browserify ({
+            entries : js
+        })
+            .transform (babelify)
+            .bundle ()
+            .pipe (source ('app.min.js'))
+            .pipe (insert.prepend (fs.readFileSync ('./node_modules/bootstrap/dist/js/bootstrap.js')))
+            .pipe (insert.prepend (fs.readFileSync ('./node_modules/jquery/dist/jquery.min.js')))
+            .pipe (streamify (uglify ()))
+            .pipe (this.gulp.dest ('./www/assets/js'));
 
-// gulp daemons watch task
-gulp.task('daemons:watch', function () {
-    gulp.watch('./app/bundles/**/*Daemon.js', ['daemons']);
-});
+    }
 
-// gulp routes watch task
-gulp.task('routes:watch', function () {
-    gulp.watch('./app/bundles/**/*Controller.js', ['routes']);
-});
-
-// gulp routes watch task
-gulp.task('menus:watch', function () {
-    gulp.watch('./app/bundles/**/*Controller.js', ['menus']);
-});
-
-// gulp routes watch task
-gulp.task('views:watch', function () {
-    gulp.watch('./app/bundles/**/*.hbs', ['views']);
-});
-
-// gulp routes watch task
-gulp.task('javascript:watch', function () {
-    gulp.watch('./app/bundles/*/resources/js/**/*.js', ['javascript']);
-});
-
-// gulp routes watch task
-gulp.task('tags:watch', function () {
-    gulp.watch('./app/bundles/*/view/**/*.tag', ['tags']);
-});
-
-// main gulp watch task
-gulp.task('watch', ['sass:watch', 'routes:watch', 'menus:watch', 'views:watch', 'tags:watch', 'javascript:watch']);
-gulp.task('install', ['sass', 'routes', 'menus', 'views', 'tags', 'javascript']);
-
-// full server task
-gulp.task('devServer', function () {
-    // Start the server at the beginning of the task
-    server.run(['./bin/server.js']);
-
-    // watch sass pipe
-    gulp.watch(['./app/bundles/**/*.scss'], function(event){
-        gulp.run('sass');
-        server.notify(event);
-    });
-
-    // watch daemons pipe
-    gulp.watch(['./app/bundles/**/*Daemon.js'], function(event){
-        gulp.run('daemons');
-        server.notify(event);
-    });
-
-    // watch routes pipe
-    gulp.watch(['./app/bundles/**/*Controller.js'], function(event){
-        gulp.run('routes');
-        server.notify(event);
-    });
-
-    // watch menus pipe
-    gulp.watch(['./app/bundles/**/*Controller.js'], function(event){
-        gulp.run('menus');
-        server.notify(event);
-    });
-
-    // watch views pipe
-    gulp.watch(['./app/bundles/**/*.hbs'], function(event) {
-        gulp.run('views');
-        server.notify(event);
-    });
-
-    // watch tags pipe
-    gulp.watch(['./app/bundles/**/*.tag'], function(event) {
-        gulp.run('tags');
-        gulp.run('javascript');
-        server.notify(event);
-    });
-
-    // watch javascript
-    gulp.watch(['./app/bundles/*/resources/js/**/*.js'], function(event) {
-        gulp.run('javascript');
-        server.notify(event);
-    });
-});
-
-// set default task devServer
-gulp.task('default', ['devServer']);
-
-/**
- * recursively merge object
- * @param obj1
- * @param obj2
- * @returns {*}
- * @constructor
- */
-function MergeRecursive(obj1, obj2) {
-    for (var p in obj2) {
-        try {
-            // Property in destination object set; update its value.
-            if (obj2[p].constructor == Object) {
-                obj1[p] = MergeRecursive(obj1[p], obj2[p]);
-            } else {
+    /**
+     * merges two objects
+     *
+     * @param obj1
+     * @param obj2
+     * @returns {*}
+     * @private
+     */
+    _merge(obj1, obj2) {
+        for (var p in obj2) {
+            try {
+                // Property in destination object set; update its value.
+                if (obj2[p].constructor == Object) {
+                    obj1[p] = this._merge (obj1[p], obj2[p]);
+                } else {
+                    obj1[p] = obj2[p];
+                }
+            } catch (e) {
+                // Property in destination object not set; create it and set its value.
                 obj1[p] = obj2[p];
             }
-        } catch (e) {
-            // Property in destination object not set; create it and set its value.
-            obj1[p] = obj2[p];
         }
-    }
 
-    return obj1;
+        return obj1;
+    }
 }
+
+/**
+ * export gulp builder class
+ *
+ * @type {gulpBuilder}
+ */
+module.exports = new gulpBuilder ();
