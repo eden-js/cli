2476d0cb2902e7e7086933deedc136441c55bd64
Alex <alex.taylor@ifactory.com.au>
2016-02-26 16:05:53 +1000

    create default daemon class

---
+++ bin/bundles/core/daemon.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Alex.Taylor on 26/02/2016.
+ */
+
+// use strict
+'use strict';
+
+/**
+ * construct daemon class
+ */
+class daemon {
+
+}
+
+/**
+ * export daemon class
+ *
+ * @type {daemon}
+ */
+module.exports = daemon;
