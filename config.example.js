1d3d2857e552a4eea2945d2952db3b368442833b
Alex-iFactory <alex.taylor@ifactory.com.au>
2016-02-27 01:16:49 +0100

    add example config file

---
+++ config.example.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Awesome on 2/27/2016.
+ */
+
+module.exports = {
+    // the port to run the app
+    port        : '3007',
+    // the environment to run the app in
+    environment : 'dev',
+    // database information (mongodb)
+    database    : {
+        dev : {
+            host : 'localhost',
+            db   : 'database'
+        }
+    }
+};
