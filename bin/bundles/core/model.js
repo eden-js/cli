96309c4fa982ea2d0cc838484ea9733a37783000
Alex <alex.taylor@ifactory.com.au>
2016-03-01 10:40:30 +1000

    allow sub-documents

--- bin/bundles/core/model.js
+++ bin/bundles/core/model.js
@@ -12,25 +12,119 @@ var mongorito = require('mongorito');
  * build model
  */
 class model extends mongorito.Model {
+    /**
+     * construct model entity
+     *
+     * @param props
+     */
     constructor(props) {
+        // run super
         super(props);
 
-        console.log(module.parent);
+        // bind attribute methods
+        this._getAttributes = this._getAttributes.bind(this);
+        this._setAttributes = this._setAttributes.bind(this);
+
+        // bind model methods
+        this._isModel   = this._isModel.bind(this);
+        this._loadModel = this._loadModel.bind(this);
+
+        // set model location
+        this._modelLocation = module.parent.filename.replace(global.appRoot, '');
+        this._loads         = {};
+
+        // set attributes before save
+        this.before ('save', '_setAttributes');
+        this.after  ('save', '_getAttributes');
+
+        // run attributes method
+        this._getAttributes();
     }
+
     /**
-     * check attributes
+     * gets models from attributes
      *
+     * @param next
      * @private
      */
-    _checkAttributes() {
+    * _getAttributes(next) {
+        // loop attributes
         for (var key in this.attributes) {
             var attr = this.attributes[key];
-            if (attr === Object(attr)) {
-                if (attr.entity) {
-                    //this.attributes[key] = require(global.appRoot + attr.entity).findById(attr.id);
+
+            // check if is object
+            if (attr === Object(attr) && attr.model) {
+                // load model
+                this._loadModel(key, attr);
+            } else if (Array.isArray(attr)) {
+                // loop object array
+                for (var i = 0; i < attr.length; i++) {
+                    // check if is object
+                    if (attr[i] === Object(attr) && attr[i].model) {
+                        this._loadModel(key + '.' + i, attr);
+                    }
                 }
             }
         }
+
+        // run next
+        yield next;
+    }
+
+    /**
+     * sets attributes
+     *
+     * @private
+     */
+    * _setAttributes(next) {
+        // loop attributes
+        for (var key in this.attributes) {
+            // set let attribute
+            let attr = this.attributes[key];
+
+            // check if entity
+            if (this._isModel(attr)) {
+                // set array for save
+                this.set(key, {
+                    'id'    : attr.get('_id').toString(),
+                    'model' : attr._modelLocation
+                });
+            }
+        }
+
+        // run next
+        yield next;
+    }
+
+    /**
+     * check if model
+     *
+     * @param obj
+     * @returns {boolean}
+     * @private
+     */
+    * _isModel(obj) {
+        // check if model
+        if (obj === Object(obj) && obj._modelLocation) {
+            return true;
+        }
+    }
+
+    /**
+     * loads model
+     *
+     * @param key
+     * @param attr
+     * @private
+     */
+    * _loadModel(key, attr) {
+        // check model loaded
+        if (!this._loads[attr.model]) {
+            this._loads[attr.model] = require(global.appRoot + attr.model);
+        }
+
+        // load by id
+        this.set(key, yield this._loads[attr.model].findById(attr.id));
     }
 }
 
