3c8b1e7bde4eef70a9392835c967718e3e0c0aa6
Alex Taylor <alex.taylor@ifactory.com.au>
2016-05-23 14:35:21 +1000

    move datagrid methods to own bundle in core

---
+++ bin/bundles/datagrid/resources/js/bootstrap.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Awesome on 2/6/2016.
+ */
+
+/**
+ * build datagrid class
+ */
+class datagrid {
+    /**
+     * construct datagrid
+     */
+    constructor () {
+        // bind datagrid methods
+        this._renderGrids = this._renderGrids.bind (this);
+
+        // render grids
+        this._renderGrids ();
+    }
+
+    /**
+     * renders grids
+     *
+     * @private
+     */
+    _renderGrids () {
+        // set default settings
+        jQuery.extend (jQuery.fn.bootgrid.Constructor.defaults.css, {
+            icon             : 'icon fa',
+            iconColumns      : 'fa-th-list',
+            iconDown         : 'fa-sort-desc',
+            iconRefresh      : 'fa-refresh',
+            iconSearch       : 'fa-search',
+            iconUp           : 'fa-sort-asc',
+            pagination       : 'pagination pagination-sm',
+            paginationButton : 'page-link'
+        });
+
+        // render grids
+        jQuery ('[data-grid]').each (function () {
+            jQuery (this).bootgrid ({
+                'ajax' : true,
+                'url'  : jQuery (this).attr ('data-grid')
+            });
+        });
+    }
+}
+
+/**
+ * export datagrid class
+ *
+ * @type {datagrid}
+ */
+module.exports = new datagrid ();
