29e5c665a57d71546674e9ab200a7e0eda407344
Alex Taylor <alex.taylor@ifactory.com.au>
2016-05-30 14:43:53 +1000

    update default app configuration for middleware

--- config.example.js
+++ config.example.js
@@ -2,55 +2,133 @@
  * Created by Awesome on 2/27/2016.
  */
 
-module.exports = {
-    // title of the app
-    title       : 'EdenFrame',
-    // domain for the app
-    domain      : 'domain.tld',
-    // the port to run the app
-    port        : '3007',
-    // the environment to run the app in
-    environment : 'dev',
-    // secret for crypto
-    secret      : 'SECRET',
-    // secret for session
-    session     : 'SESSION',
-    // set threads
-    threads     : 1,
-    // default acl information
-    acl         : {
-        // default acl for every user
-        'default' : {
-            name  : 'user',
-            value : [
-                'loggedIn'
-            ]
-        },
-        // default acl for first user (admin)
-        'first'   : {
-            name  : 'admin',
-            value : true
-        }
+/**
+ * create initial config Object
+ *
+ * @type {Object}
+ */
+var config = {};
+
+
+/**
+ * set application page information
+ */
+
+// set application title
+config.title  = 'Baseplan API';
+
+// set application domain
+config.domain = 'baseplan.api.dev';
+
+
+/**
+ * set application server configuration
+ */
+
+// set starting port
+config.port = '3001';
+
+// set amount of instances to run
+// setting this as null will count your CPU cores
+config.threads = 1;
+
+// should the app use websockets
+config.socket = true;
+
+// set app environment
+config.environment = 'dev';
+
+
+/**
+ * set database configuration
+ */
+
+// set config database object
+config.database = {
+    // dev database
+    dev : {
+        host : 'localhost',
+        db   : 'baseplanApi'
     },
-    // database information (mongodb)
-    database    : {
-        dev : {
-            host : 'localhost',
-            db   : 'database'
-        }
+    // set live database
+    live : {
+        host : 'localhost',
+        db   : 'baseplanApi'
+    }
+};
+
+
+/**
+ * set included files
+ */
+
+// set scss imports
+// these are imported into app.min.css by default
+config.sass = [
+    './node_modules/bootstrap/scss/bootstrap-flex.scss',
+    './node_modules/tether/src/css/tether.scss'
+];
+
+// set js imports
+// these are imported into app.min.js at the top
+config.js = [
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/tether/dist/js/tether.min.js',
+    './node_modules/jquery-bootgrid/dist/jquery.bootgrid.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.js'
+];
+
+
+/**
+ * set application session secret
+ */
+
+// secret for crypto
+config.secret = 'x:.+o85I6d6b1N1wrU.2*~I1299Hg23Hf^H1pRX5t!UAqR.T5S';
+
+// secret for session
+config.session = 'x:.+o85I6d6b1N1wrU.2*~I1299Hg23Hf^H1pRX5t!UAqR.T5S';
+
+
+/**
+ * set default ACL information
+ */
+
+// set acl object
+config.acl = {
+    // default acl per user
+    'default' : {
+        name  : 'user',
+        value : [
+            'loggedIn'
+        ]
     },
-    // default sass files
-    sass         : [
-        './node_modules/bootstrap/scss/bootstrap-flex.scss',
-        './node_modules/tether/src/css/tether.scss',
-        './node_modules/toastr/toastr.scss'
-    ],
-    // default javascript files
-    js           : [
-        './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/tether/dist/js/tether.min.js',
-        './node_modules/jquery-bootgrid/dist/jquery.bootgrid.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.js',
-        './node_modules/toastr/build/toastr.min.js'
+    // default acl for first user (admin)
+    'first'   : {
+        name  : 'admin',
+        value : true
+    }
+};
+
+
+/**
+ * set view import functionality
+ */
+
+// create view object
+config.view = {
+    // modules will be required at the top of riots tags.min.js
+    'include' : [
+        // include riot module
+        'riot',
+        // include socket module
+        'socket'
     ]
 };
+
+/**
+ * export config
+ *
+ * @type {Object}
+ */
+module.exports = config;
