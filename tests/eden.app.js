627d28e5428a26b7515235bdb310839e13de0050
Alex Taylor <alex.taylor@ifactory.com.au>
2016-07-21 10:25:39 +1000

    complete testing on express app

---
+++ tests/eden.app.js
@@ -0,0 +1,17 @@
+// use strict
+'use strict';
+
+// create eden
+var assert  = require ('assert');
+
+// describe eden
+describe ('EdenJS Express App', () => {
+  // test standard event emitter functionality
+  it ('should pass express as app', (done) => {
+    // assert express
+    assert.equal (typeof global.eden.app, 'function');
+
+    // run done
+    done ();
+  });
+});
