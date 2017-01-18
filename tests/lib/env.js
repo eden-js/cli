e11fbbdec601f2ed4290ff1f9486d7aaac4fd4d7
Alex Taylor <alex.taylor@ifactory.com.au>
2017-01-18 14:13:49 +1000

    (bug) amend issues with test suite

--- tests/lib/env.js
+++ tests/lib/env.js
@@ -6,15 +6,23 @@
 // require dependencies
 var os      = require ('os');
 var path    = require ('path');
+var addPath = require ('app-module-path').addPath;
 var winston = require ('winston');
 
 // set global app root
 global.appRoot = path.dirname (path.dirname (__dirname));
 
+// add node paths
+addPath (global.appRoot);
+addPath (global.appRoot + '/lib/core');
+addPath (global.appRoot + '/app/bundles');
+addPath (global.appRoot + '/lib/bundles');
+addPath (global.appRoot + '/lib/aliases');
+
 // require local dependencies
-var log    = require (global.appRoot + '/lib/utilities/log');
-var eden   = require (global.appRoot + '/lib/eden');
-var config = require (global.appRoot + '/app/config');
+var log    = require ('lib/utilities/log');
+var eden   = require ('lib/eden');
+var config = require ('app/config');
 
 // set global environment
 global.testing     = true;
