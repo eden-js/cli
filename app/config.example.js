f8c2d75216a4e1f9f738c07197a65c5923fc5471
Alex Taylor <alex.taylor@ifactory.com.au>
2017-10-13 09:37:05 +1000

    remove riotcontrol and move window.eden to use the eden store

--- app/config.example.js
+++ app/config.example.js
@@ -127,7 +127,6 @@ config.sass = [
 // These are imported into app.min.js at the top
 config.js = [
   './node_modules/whatwg-fetch/fetch.js',
-  './node_modules/promise-polyfill/promise.min.js',
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/popper.js/dist/umd/popper.min.js',
   './node_modules/bootstrap/dist/js/bootstrap.js'
