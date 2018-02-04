ad61d2e95031376808d06854727c1023cd8a6eac
OhYea777 <jacktaylor12345@gmail.com>
2018-02-04 16:12:10 +1300

    Fix 'promise-polyfill' dependency and update 'config.example.js' to new location

--- app/config.example.js
+++ app/config.example.js
@@ -3,7 +3,7 @@
  *
  * @type {Object}
  */
-var config = {};
+const config = {};
 
 
 /**
@@ -122,7 +122,7 @@ config.sass = [
 // These are imported into app.min.js at the top
 config.js = [
   './node_modules/whatwg-fetch/fetch.js',
-  './node_modules/promise-polyfill/promise.min.js',
+  './node_modules/promise-polyfill/dist/promise.min.js',
   './node_modules/jquery/dist/jquery.min.js',
   './node_modules/popper.js/dist/umd/popper.min.js',
   './node_modules/bootstrap/dist/js/bootstrap.js'
